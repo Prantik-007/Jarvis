ead4909b58a3420be77b7041719061126e64400c
Prantik-007 <137792933+Prantik-007@users.noreply.github.com>
2024-08-18 14:05:06 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "Alone": "https://www.youtube.com/watch?v=TrnEmXSuKk8&pp=ygUFYWxvbmU%3D",
+    "Animals": "https://www.youtube.com/watch?v=0GVExpdmoDs&pp=ygUXYW5pbWFscyBtYXJvb24gNSBseXJpY3M%3D",
+    "Big Dawgs": "https://www.youtube.com/watch?v=hOHKltAiKXQ&pp=ygUJYmlnIGRhd2dz"
+}
